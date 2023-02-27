d87cc68c182cbc56b236e6f07a905655d77b9471
Ivan Efimov <e.1@mail.ru>
2023-02-27 23:28:43 +0600

    fix: log levels

--- src/logger/logger.constants.ts
+++ src/logger/logger.constants.ts
@@ -7,12 +7,12 @@ export const HIDE_CREDENTIALS = process.env.HIDE_CREDENTIALS === 'true';
 export const LOG_LIMIT =
   (Math.max(Number(process.env.LOG_LIMIT), 1) || 1) * 2 ** 10;
 
-export const LOG_LEVEL = Number(process.env.LOG_LEVEL) || 3;
+export const LOG_LEVEL = Number(process.env.LOG_LEVEL);
 
 export const ACTIVE_LOG_LEVELS: LogLevels[] = [
-  LogLevels.ERROR,
-  LogLevels.WARN,
-  LogLevels.LOG,
   LogLevels.DEBUG,
   LogLevels.VERBOSE,
-].slice(0, LOG_LEVEL);
+  LogLevels.LOG,
+  LogLevels.WARN,
+  LogLevels.ERROR,
+].slice(LOG_LEVEL);
