00ab91309468de2b17cca10f013e4e6b0b3ad683
Ivan Efimov <e.1@mail.ru>
2023-03-03 10:36:20 +0600

    fix: env destructuring

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,6 +1,4 @@
-export const {
-  TOKEN_EXPIRE_TIME,
-  TOKEN_REFRESH_EXPIRE_TIME,
-  JWT_SECRET_KEY,
-  JWT_SECRET_REFRESH_KEY,
-} = process.env;
+export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME;
+export const TOKEN_REFRESH_EXPIRE_TIME = process.env.TOKEN_REFRESH_EXPIRE_TIME;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+export const JWT_SECRET_REFRESH_KEY = process.env.JWT_SECRET_REFRESH_KEY;
